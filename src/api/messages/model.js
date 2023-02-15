4e277c8c5894d388bdf261ac7d50a33b5e6f9cf1
Nic <nic.dan.costea@gmail.com>
2023-02-15 18:19:57 +0100

    date in model

--- src/api/messages/model.js
+++ src/api/messages/model.js
@@ -5,10 +5,39 @@ const MessageSchema = new Schema(
   {
     sender: { type: String, required: true },
     text: { type: String, required: true },
-    createdAt: { type: Date, required: false },
+    // createdAt: { type: Date, required: true },
+    createdAt: {
+      type: String,
+      default: () =>
+        new Date().toLocaleString("en-US", {
+          year: "numeric",
+          month: "2-digit",
+          day: "2-digit",
+          hour: "2-digit",
+          minute: "2-digit",
+          second: "2-digit",
+        }),
+    },
     id: { type: String, required: false },
   },
-  { timestamps: true }
+  { timestamps: false }
 );
+MessageSchema.pre("save", function (next) {
+  if (this.createdAt) {
+    const formattedDate = new Date(this.createdAt).toLocaleString("en-US", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    });
+    console.log("formattedDate: ", formattedDate);
+    this.createdAt = formattedDate;
+    console.log("this.createdAt: ", this.createdAt);
+  }
+  console.log("THIS from the MessageSchema: ", this);
+  next();
+});
 
 export default model("Message", MessageSchema);
