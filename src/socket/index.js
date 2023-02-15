fa1de7c66e55175766bd0e221f4a624824d19ead
Nic <nic.dan.costea@gmail.com>
2023-02-15 16:12:01 +0100

    users - listening and emitting

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,36 +1,37 @@
-let onlineUsers = [];
+let users = [];
 
-export const newConnectionHandler = (newClient) => {
-  console.log("NEW CONNECTION:", newClient.id);
+export const newConnectionHandler = (client) => {
+  console.log("NEW CONNECTION - id: ", client.id);
 
   // 1. Emit a "welcome" event to the connected client
-  newClient.emit("welcome", { message: `Hello ${newClient.id}` });
+  client.emit("greetOnMount", { message: `Hello ${client.id}` });
 
-  // 2. Listen to an event emitted by the FE called "setUsername", this event is going to contain the username in the payload
-  newClient.on("setUsername", (payload) => {
+  // 2. Listen to an event emitted by the FE called "submitUserName", this event is going to contain the username in the payload
+  client.on("submitUserName", (payload) => {
     console.log(payload);
     // 2.1 Whenever we receive the username, we keep track of that together with the socket.id
-    onlineUsers.push({ username: payload.username, socketId: newClient.id });
+    users.push({ username: payload.username, socketId: client.id });
 
     // 2.2 Then we have to send the list of online users to the current user that just logged in
-    newClient.emit("loggedIn", onlineUsers);
+    client.emit("usersLoggedIn", users);
 
     // 2.3 We have also to inform everybody (but not the sender) of the new user which just joined
-    newClient.broadcast.emit("updateOnlineUsersList", onlineUsers);
+    client.broadcast.emit("updateUsersList", users);
+    console.log("the updated users list: ", users);
   });
 
   // 3. Listen to "sendMessage" event, this is received when an user sends a new message
-  newClient.on("sendMessage", (message) => {
+  client.on("sendMessage", (message) => {
     console.log("NEW MESSAGE:", message);
     // 3.1 Whenever we receive that new message we have to propagate that message to everybody but not sender
-    newClient.broadcast.emit("newMessage", message);
+    client.broadcast.emit("newMessage", message);
   });
 
   // 4. Listen to an event called "disconnect", this is NOT a custom event!! This event happens when an user closes browser/tab
-  newClient.on("disconnect", () => {
-    // 4.1 Server shall update the list of onlineUsers by removing the one that has disconnected
-    onlineUsers = onlineUsers.filter((user) => user.socketId !== newClient.id);
+  client.on("disconnect", () => {
+    // 4.1 Server shall update the list of users by removing the one that has disconnected
+    users = users.filter((user) => user.socketId !== client.id);
     // 4.2 Let's communicate the updated list all the remaining clients
-    newClient.broadcast.emit("updateOnlineUsersList", onlineUsers);
+    client.broadcast.emit("updateusersList", users);
   });
 };
